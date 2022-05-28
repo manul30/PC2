3bc056e0c3456a60179aade17f927258a36821ae
manuel30 <manuelocc30@gmail.com>
2022-05-28 09:53:38 -0500

    .

---
+++ routes/estudiantes.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var estudiante = require('../controllers/EstudianteController.js');
+router.get('/', estudiante.list);
+router.get('/show/:id', estudiante.show);
+router.get('/create', estudiante.create);
+router.post('/save', estudiante.save);
+router.get('/edit/:id', estudiante.edit);
+router.post('/update/:id', estudiante.update);
+router.post('/delete/:id', estudiante.delete);
+module.exports = router;
