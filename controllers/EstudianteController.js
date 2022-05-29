bd38ed55b35ef6b7f7457308f20d18c7a470f446
manuel30 <manuelocc30@gmail.com>
2022-05-28 22:39:58 -0500

    Listar y agregar estudiantes

--- controllers/EstudianteController.js
+++ controllers/EstudianteController.js
@@ -35,48 +35,5 @@ estudianteController.save = function(req, res){
  //res.redirect("/estudiantes");
  });
 };
-estudianteController.edit = function(req, res) {
- Estudiante.findOne({_id: req.params.id}).exec(function (err, estudiante)
-{
- if (err) { console.log("Error:", err); return; }
 
- res.render("../views/estudiante/edit", {estudiante: estudiante});
-
- });
-};
-estudianteController.update = function(req, res){
- Estudiante.findByIdAndUpdate( req.params.id, {$set: {
-
-    codigo: req.body.codigo,
-    descripcion: req.body.descripcion,
-    precio: req.body.precio,
-    stock: req.body.stock,
-    marca: req.body.marca,
-    modelo: req.body.modelo,
-    categoria: req.body.categoria,
-    fecha_produccion: req.body.fecha_produccion
- }}, { new: true },
- function( err, estudiante){
- if( err ){
- console.log('Error: ', err);
- res.render('../views/estudiante/edit', {estudiante: req.body}
-);
- }
-
- console.log( estudiante );
-
- res.redirect('/estudiantes/show/' + estudiante._id);
-
- });
-};
-estudianteController.delete = function(req, res){
-
- Estudiante.remove({_id: req.params.id}, function(err){
- if( err ){ console.log('Error: ', err); return; }
-
- console.log("Estudiante deleted!");
- res.redirect("/estudiantes");
- });
-
-};
 module.exports = estudianteController;
--- routes/estudiantes.js
+++ routes/estudiantes.js
@@ -5,7 +5,4 @@ router.get('/', estudiante.list);
 router.get('/show/:id', estudiante.show);
 router.get('/create', estudiante.create);
 router.post('/save', estudiante.save);
-router.get('/edit/:id', estudiante.edit);
-router.post('/update/:id', estudiante.update);
-router.post('/delete/:id', estudiante.delete);
 module.exports = router;
