1a99cc8e60181c0cd1338c77642ca1215e2cbf7e
manuel30 <manuelocc30@gmail.com>
2022-05-29 12:41:15 -0500

    agregar lineas

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-var estudiantesRouter = require('./routes/estudiantes');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,10 +32,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+var estudiantesRouter = require('./routes/estudiantes');
+app.use('/estudiantes', estudiantesRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/estudiantes', estudiantesRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
